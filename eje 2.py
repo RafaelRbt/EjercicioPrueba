4f4030a548afbcffa0dab459ec78ad6237e5c9d2
RafaelRbt <46898196+RafaelRbt@users.noreply.github.com>
2019-01-21 09:43:29 -0600

    Romero B, Rafael, Grupo 02

---
+++ eje 2.py
@@ -0,0 +1,6 @@
+#Autor:Rafael Romero Bello
+#Este programa calcula la velocidad promedio en un viaje
+tiempo=int(input("teclea el tiempo de viaje: "))
+distancia=int(input("teclea la distancia del viaje:"))
+velocidad=distancia/tiempo
+print("la velocidad promedio es:", velocidad)
